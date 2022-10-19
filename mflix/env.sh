1f7f20452e41d963e3375fd9eaefc1bcab7d4168
Wriddhirup Dutta <iwriddhirupd147@gmail.com>
2022-10-19 18:51:04 +0530

    first commit

---
+++ mflix/env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export FLASK_APP=mflix/mflix.py
+export FLASK_DEBUG=false
+export MFLIX_DB_URI="mongodb+srv://analytics:analytics-password@analytics.zadpxix.mongodb.net/?retryWrites=true&w=majority" # Replace XXXX with your MongoDB Connection URI
